4eeab5f5bbe3a359f787c72b54a62d023c517b1e
Shreya <shreyagupta4563@gmail.com>
2022-07-10 17:09:27 +0530

    added a file

---
+++ sumofdigits.py
@@ -0,0 +1,11 @@
+n=int(input("Enter a number: "));
+rev=0;
+s1=0;
+while(n>0):
+    a=n%10;
+    rev=(rev*10)+a;
+    s1+=a;
+    n=n//10;
+    
+print("Reverse of digits is:",rev);
+print("Sum of Digits is:",s1);
